9b58ce0b6400764f443320af4be837c196eb0b69
Weerayut Hongsa <kusumoto.com@gmail.com>
2019-06-03 23:17:51 +0700

    Add support multiSortMeta in primeng grid filter and refactor code

--- PrimeNG.TableFilter/PrimeNGTableFilterExtension.cs
+++ PrimeNG.TableFilter/PrimeNGTableFilterExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using PrimeNG.TableFilter.Models;
 using PrimeNG.TableFilter.Utils;
 
@@ -16,52 +17,96 @@ namespace PrimeNG.TableFilter
         {
             foreach (var filter in tableFilterPayload.Filters)
             {
-                switch (filter.Value.MatchMode)
-                {
-                    case FilterTypeMatchModeStartsWith:
-                        dataSet = dataSet.WhereWithExtensions("StartsWith", filter.Key.FirstCharToUpper(), filter.Value.Value);
-                        break;
-
-                    case FilterTypeMatchModeContains:
-                        dataSet = dataSet.WhereWithExtensions("Contains", filter.Key.FirstCharToUpper(), filter.Value.Value);
-                        break;
-
-                    case FilterTypeMatchModeIn:
-                        dataSet = dataSet.WhereWithList(filter.Key.FirstCharToUpper(), filter.Value.Value);
-                        break;
-
-                    case FilterTypeMatchModeEndsWith:
-                        dataSet = dataSet.WhereWithExtensions("EndsWith", filter.Key.FirstCharToUpper(), filter.Value.Value);
-                        break;
-
-                    case FilterTypeMatchModeEquals:
-                        dataSet = dataSet.WhereWithExtensions("Equals", filter.Key.FirstCharToUpper(), filter.Value.Value);
-                        break;
-
-                    default:
-                        throw new System.ArgumentException("Match mode is invalid");
-                }
+                dataSet = FilterDataSet(dataSet, filter);
             }
             totalRecord = dataSet.Count();
             if (!string.IsNullOrEmpty(tableFilterPayload.SortField))
             {
-                switch (tableFilterPayload.SortOrder)
-                {
-                    case (int)SortingEnumeration.OrderByAsc:
-                        dataSet = dataSet.OrderBy(tableFilterPayload.SortField.FirstCharToUpper(), false);
-                        break;
-
-                    case (int)SortingEnumeration.OrderByDesc:
-                        dataSet = dataSet.OrderBy(tableFilterPayload.SortField.FirstCharToUpper(), true);
-                        break;
-
-                    default:
-                        throw new System.ArgumentException("Sort Order is invalid");
-                }
+                dataSet = SingleOrderDataSet(dataSet, tableFilterPayload);
+            }
+            if (tableFilterPayload.MultiSortMeta != null && tableFilterPayload.MultiSortMeta.Any())
+            {
+                dataSet = MultipleOrderDataSet(dataSet, tableFilterPayload);
             }
             dataSet = dataSet.Skip(tableFilterPayload.First).Take(tableFilterPayload.Rows);
             return dataSet;
         }
 
+        private static IQueryable<T> MultipleOrderDataSet<T>(IQueryable<T> dataSet, TableFilterModel tableFilterPayload)
+        {
+            tableFilterPayload.MultiSortMeta.Select((value, i) => new { i, value }).ToList().ForEach(o =>
+            {
+                switch (o.value.Order)
+                {
+                    case (int)SortingEnumeration.OrderByAsc:
+                        if (o.i == 0)
+                            dataSet = dataSet.OrderBy(o.value.Field.FirstCharToUpper(), false);
+                        else
+                            dataSet = dataSet.ThenOrderBy(o.value.Field.FirstCharToUpper(), false);
+                        break;
+
+                    case (int)SortingEnumeration.OrderByDesc:
+                        if (o.i == 0)
+                            dataSet = dataSet.OrderBy(o.value.Field.FirstCharToUpper(), true);
+                        else
+                            dataSet = dataSet.ThenOrderBy(o.value.Field.FirstCharToUpper(), true);
+                        break;
+
+                    default:
+                        throw new System.ArgumentException("Sort Order is invalid");
+                }
+            });
+            return dataSet;
+        }
+
+        private static IQueryable<T> SingleOrderDataSet<T>(IQueryable<T> dataSet, TableFilterModel tableFilterPayload)
+        {
+            switch (tableFilterPayload.SortOrder)
+            {
+                case (int)SortingEnumeration.OrderByAsc:
+                    dataSet = dataSet.OrderBy(tableFilterPayload.SortField.FirstCharToUpper(), false);
+                    break;
+
+                case (int)SortingEnumeration.OrderByDesc:
+                    dataSet = dataSet.OrderBy(tableFilterPayload.SortField.FirstCharToUpper(), true);
+                    break;
+
+                default:
+                    throw new System.ArgumentException("Sort Order is invalid");
+            }
+
+            return dataSet;
+        }
+
+        private static IQueryable<T> FilterDataSet<T>(IQueryable<T> dataSet, KeyValuePair<string, TableFilterContext> filter)
+        {
+            switch (filter.Value.MatchMode)
+            {
+                case FilterTypeMatchModeStartsWith:
+                    dataSet = dataSet.WhereWithExtensions("StartsWith", filter.Key.FirstCharToUpper(), filter.Value.Value);
+                    break;
+
+                case FilterTypeMatchModeContains:
+                    dataSet = dataSet.WhereWithExtensions("Contains", filter.Key.FirstCharToUpper(), filter.Value.Value);
+                    break;
+
+                case FilterTypeMatchModeIn:
+                    dataSet = dataSet.WhereWithList(filter.Key.FirstCharToUpper(), filter.Value.Value);
+                    break;
+
+                case FilterTypeMatchModeEndsWith:
+                    dataSet = dataSet.WhereWithExtensions("EndsWith", filter.Key.FirstCharToUpper(), filter.Value.Value);
+                    break;
+
+                case FilterTypeMatchModeEquals:
+                    dataSet = dataSet.WhereWithExtensions("Equals", filter.Key.FirstCharToUpper(), filter.Value.Value);
+                    break;
+
+                default:
+                    throw new System.ArgumentException("Match mode is invalid");
+            }
+
+            return dataSet;
+        }
     }
 }
--- PrimeNG.TableFilter/Utils/DynamicOrderByUtil.cs
+++ PrimeNG.TableFilter/Utils/DynamicOrderByUtil.cs
@@ -19,5 +19,19 @@ namespace PrimeNG.TableFilter.Utils
                 source.Expression, Expression.Quote(orderByExpression));
             return source.Provider.CreateQuery<TEntity>(resultExpression);
         }
+
+        public static IQueryable<TEntity> ThenOrderBy<TEntity>(this IQueryable<TEntity> source, string orderByProperty,
+            bool desc)
+        {
+            var command = desc ? "ThenByDescending" : "ThenBy";
+            var type = typeof(TEntity);
+            var property = type.GetProperty(orderByProperty);
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property ?? throw new InvalidOperationException());
+            var orderByExpression = Expression.Lambda(propertyAccess, parameter);
+            var resultExpression = Expression.Call(typeof(Queryable), command, new[] { type, property.PropertyType },
+                source.Expression, Expression.Quote(orderByExpression));
+            return source.Provider.CreateQuery<TEntity>(resultExpression);
+        }
     }
 }
