4a7b36a3088ef450e832ef328217ae1a769de2e2
karthikkumar <karthiksubramaniam@live.com>
2023-02-18 22:03:24 +0530

    Global filter implementation

--- PrimeNG.TableFilter/Models/TableFilterModel.cs
+++ PrimeNG.TableFilter/Models/TableFilterModel.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
 
 namespace PrimeNG.TableFilter.Models
 {
-    public class TableFilterModel
-    {
-        public Dictionary<string, object> Filters { get; set; }
-        public int First { get; set; }
-        public int Rows { get; set; }
-        public string SortField { get; set; }
-        public int SortOrder { get; set; }
-        public List<TableFilterSortMeta> MultiSortMeta { get; set; }
-    }
+	public class TableFilterModel
+	{
+		public Dictionary<string, object> Filters { get; set; }
+		public int First { get; set; }
+		public int Rows { get; set; }
+		public string SortField { get; set; }
+		public int SortOrder { get; set; }
+		public List<TableFilterSortMeta> MultiSortMeta { get; set; }
+		public string GlobalFilter { get; set; }
+	}
 }
